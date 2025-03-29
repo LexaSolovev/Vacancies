443d1f503b2ed127aa7129c385b32627bad5105c
Alexey Solovyev <solovaa@yandex.ru>
2025-03-29 21:21:11 +0000

    add class Salary

---
+++ src/Salary.py
@@ -0,0 +1,11 @@
+class Salary:
+    def __init__(self, salary_from=0, salary_to=0, currency='RUR'):
+        self.salary_from = salary_from
+        self.salary_to = salary_to
+        self.currency = currency
+
+    def __str__(self):
+        if self.salary_from and self.salary_to:
+            return f'{self.salary_from} - {self.salary_to} {self.currency}'
+        else:
+            return 'Зарплата не указана'
