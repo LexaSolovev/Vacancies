7382901b9732bb3ed31494cc308e0a5738e1fcc7
Alexey Solovyev <solovaa@yandex.ru>
2025-03-29 08:41:27 +0000

    separate class HeadHunterAPI to new file

--- src/VacancyAPI.py
+++ src/VacancyAPI.py
@@ -1,32 +1,7 @@
 from abc import ABC, abstractmethod
 
-import requests
-
-
 class VacancyAPI(ABC):
 
     @abstractmethod
     def get_vacancies(self, query: str):
         pass
-
-
-class HeadHunterAPI(VacancyAPI):
-
-    def __init__(self):
-        self.url = 'https://api.hh.ru/vacancies'
-        self.headers = {'User-Agent': 'HH-User-Agent'}
-        self.params = {'text': '', 'page': 0, 'per_page': 100, 'city': 'Москва'}
-        self.vacancies = []
-
-    def get_vacancies(self, keyword):
-        self.params['text'] = keyword
-        while self.params.get('page') != 20:
-            response = requests.get(self.url, headers=self.headers, params=self.params)
-            vacancies = response.json()['items']
-            self.vacancies.extend(vacancies)
-            self.params['page'] += 1
-
-if __name__ == "__main__":
-    hh = HeadHunterAPI()
-    hh.get_vacancies("Python")
-    print(hh.vacancies)
