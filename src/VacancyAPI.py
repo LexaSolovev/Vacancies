bc3557c057388544c7166176e42036b9dba57640
Alexey Solovyev <solovaa@yandex.ru>
2025-03-29 23:02:26 +0000

    fix

--- src/VacancyAPI.py
+++ src/VacancyAPI.py
@@ -3,7 +3,7 @@ from abc import ABC, abstractmethod
 class VacancyAPI(ABC):
 
     @abstractmethod
-    def get_vacancies(self):
+    def get_vacancies(self, params):
         pass
 
     @abstractmethod
